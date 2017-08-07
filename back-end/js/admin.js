d817dca19729c27fb73b48530057526295ee5f14
Claudio Campos <claudio.rabelo@fttx.grupo>
2017-08-07 11:54:04 -0300

    melhoramento de bugs

--- back-end/js/admin.js
+++ back-end/js/admin.js
@@ -7,9 +7,22 @@ jQuery(function(){
 	jQuery('.acf-field-the-contents').parent().parent().parent().find('.term-description-wrap').hide();
 	jQuery('#edittag').fadeIn();
 
-	jQuery(".acf-table .acf-field[data-name='tamanhos_grid'] ul li input").click(function(){
-		jQuery('.acf-field-flexible-content')[0].className = jQuery('.acf-field-flexible-content')[0].className.replace(/\bcolumn_.*?\b/g, '');
-		jQuery('.acf-field-flexible-content').addClass('column_'+jQuery(this).val());
-	});
+	set_column();
 
 });
+
+function set_column(){
+
+	jQuery(".acf-table .acf-field[data-name='tamanhos_grid'] ul li input, .button[data-event='add-row']").click(function(){
+		var parents = jQuery(this).parent().parent().parent().parent().parent().parent();
+		console.log(parents);
+		parents.find('.acf-field-flexible-content')[0].className = parents.find('.acf-field-flexible-content')[0].className.replace(/\bcolumn_.*?\b/g, '');
+		parents.find('.acf-field-flexible-content').addClass('column_'+jQuery(this).val());
+		if(jQuery(this).attr('data-event')=='add-row'){
+			setTimeout(function(){
+				set_column();
+			},1000);
+		}
+	});
+
+}
