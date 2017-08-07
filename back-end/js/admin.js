04ffc96d441417bede50368ae74eca4dfd4086cd
Claudio Campos <claudio.rabelo@fttx.grupo>
2017-08-07 14:57:16 -0300

    correcoes save

--- back-end/js/admin.js
+++ back-end/js/admin.js
@@ -1,13 +1,15 @@
 jQuery(function(){
+	
 	jQuery('.acf-field-the-contents').parent().parent().parent().parent().parent().find('.postarea').hide();
 	jQuery('.acf-field-the-contents').parent().parent().parent().parent().parent().find('#pageparentdiv').hide();
 	jQuery('.acf-field-the-contents').parent().parent().parent().parent().parent().find('#postimagediv').hide();
-	jQuery('#poststuff').fadeIn();
+	jQuery('#poststuff').fadeIn(function(){
+		set_column_load();
+		set_column();
+	});
 
 	jQuery('.acf-field-the-contents').parent().parent().parent().find('.term-description-wrap').hide();
 	jQuery('#edittag').fadeIn();
-
-	set_column();
 
 });
 
@@ -23,6 +25,19 @@ function set_column(){
 				set_column();
 			},1000);
 		}
+	});
+
+}
+
+function set_column_load(){
+
+	jQuery(".acf-table .acf-field[data-name='tamanhos_grid']").each(function(){
+		console.log();
+		var parents = jQuery(this).parent().parent();
+		console.log(parents);
+		parents.find('.acf-field-flexible-content')[0].className = parents.find('.acf-field-flexible-content')[0].className.replace(/\bcolumn_.*?\b/g, '');
+		parents.find('.acf-field-flexible-content').addClass('column_'+jQuery(this).find('ul li input:checked').val());
+		
 	});
 
 }
