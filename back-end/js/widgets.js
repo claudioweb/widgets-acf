a1266193e5bd38ef9114b92205944e5d56b3833b
Claudio Campos <claudio.rabelo@fttx.grupo>
2017-08-11 18:34:34 -0300

    colorpicker acf

--- back-end/js/widgets.js
+++ back-end/js/widgets.js
@@ -36,32 +36,58 @@ function set_widget_light_box(){
 			'</h1><div class="close fa fa-times"></div>'+values_input.html()
 			);
 
-		jQuery('.acf_box_widgets_content').find('input').change(function(){
-			var name = jQuery(this).attr('name');
-			var img_new = jQuery(this).parent().find('.show-if-value img').attr('src');
-			jQuery(this_click).parent().find('.show-if-value img').attr('src',img_new);
-			jQuery(this_click).find('[name="'+name+'"]').attr('value',jQuery(this).val());
-			console.log(jQuery(this_click).find('[name="'+name+'"]').val());
-		});
+		jQuery('.acf_box_widgets_content .wp-picker-container').remove();
 
-		jQuery('.acf_box_widgets_content').find('select').select2();
+		var myOptions = {
+    		// you can declare a default color here,
+    		// or in the data-default-color attribute on the input
+    		defaultColor: false,
+    		// a callback to fire whenever the color changes to a valid color
+    		change: function(event, ui){
+    			console.log(jQuery(this));
+    			var name = jQuery(this).attr('name');
+    			var img_new = jQuery(this).parent().find('.show-if-value img').attr('src');
+    			jQuery(this_click).parent().find('.show-if-value img').attr('src',img_new);
+    			jQuery(this_click).find('[name="'+name+'"]').attr('value',jQuery(this).val());
+    			console.log(jQuery(this_click).find('[name="'+name+'"]').val());
+    		},
+    		// a callback to fire when the input is emptied or an invalid color
+    		clear: function() {},
+    		// hide the color picker controls on load
+    		hide: true,
+    		// show a group of common colors beneath the square
+    		// or, supply an array of colors to customize further
+    		palettes: true
+    	};
 
-		jQuery('.acf_box_widgets_content').find('select').change(function(){
-			var name = jQuery(this).attr('name');
-			jQuery(this_click).find('[name="'+name+'"] option:selected').removeAttr("selected");
+    	jQuery('.acf_box_widgets_content .acf-color_picker').find('input').wpColorPicker(myOptions);
 
-			for (var i = 0; i < jQuery(this).val().length; i++) {
-				jQuery(this_click).find('[name="'+name+'"] option[value="'+jQuery(this).val()[i]+'"]').attr("selected","selected");
-			}
+    	jQuery('.acf_box_widgets_content').find('input').change(function(){
+    		var name = jQuery(this).attr('name');
+    		var img_new = jQuery(this).parent().find('.show-if-value img').attr('src');
+    		jQuery(this_click).parent().find('.show-if-value img').attr('src',img_new);
+    		jQuery(this_click).find('[name="'+name+'"]').attr('value',jQuery(this).val());
+    		console.log(jQuery(this_click).find('[name="'+name+'"]').val());
+    	});
 
-			jQuery(this_click).find('[name="'+name+'"]').parent().find('input').attr('value',jQuery(this).val());
-			console.log(jQuery(this).val());
-		});
+    	jQuery('.acf_box_widgets_content').find('select').select2();
 
-		jQuery(".acf_box_widgets_content .close").click(function(){
-			jQuery('#widget_acf_box_light').remove();
-		});
+    	jQuery('.acf_box_widgets_content').find('select').change(function(){
+    		var name = jQuery(this).attr('name');
+    		jQuery(this_click).find('[name="'+name+'"] option:selected').removeAttr("selected");
 
-	});
+    		for (var i = 0; i < jQuery(this).val().length; i++) {
+    			jQuery(this_click).find('[name="'+name+'"] option[value="'+jQuery(this).val()[i]+'"]').attr("selected","selected");
+    		}
+
+    		jQuery(this_click).find('[name="'+name+'"]').parent().find('input').attr('value',jQuery(this).val());
+    		console.log(jQuery(this).val());
+    	});
+
+    	jQuery(".acf_box_widgets_content .close").click(function(){
+    		jQuery('#widget_acf_box_light').remove();
+    	});
+
+    });
 
 }
