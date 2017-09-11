a4a4a0503bc4c72ba6cf8d50147cddb018463138
Claudio Campos <claudio.rabelo@fttx.grupo>
2017-09-11 10:20:54 -0300

    plugins necessario

---
+++ more-widgets-templates/widget-custom/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+$fields['icon'] = 'fa fa-picture-o';
+
+$fields['image']['label'] = 'Imagem destacada';
+$fields['color_picker']['label'] = 'Cor';
+$fields['text']['label'] = 'Título do widget';
+$fields['text__1']['label'] = 'Link URL';
---
+++ more-widgets-templates/widget-custom/index.php
@@ -0,0 +1,24 @@
+<?php//var_dump($fields); ?>
+<div class="row">
+	<div class="col-sm-12">
+		<div class="image_widget_custom">
+			<?php $img = $fields['field_image_widget_custom_key']; ?>
+			<?php if(!empty($img)): ?>
+
+				<?php if(is_array($img)){
+					$img = $img['sizes']['medium'];
+				}else{
+					$img = wp_get_attachment_url($img);
+				} ?>
+
+				<img src="<?php echo $img; ?>" class="img-responsive" alt="<?php echo $fields['field_text_widget_custom_key']; ?>">
+			<?php endif; ?>
+			<div class="caption_widget_custom" 
+			style="background: <?php echo $fields['field_color_picker_widget_custom_key']; ?>;">
+			<a href="<?php echo $fields['field_text__1_widget_custom_key']; ?>">
+				<?php echo $fields['field_text_widget_custom_key']; ?>
+			</a>
+		</div>
+	</div>
+</div>
+</div>
